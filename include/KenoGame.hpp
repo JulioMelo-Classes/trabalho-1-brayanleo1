b9c4d595e0d398b2e282f7470841eb698677d030
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-07-04 18:04:39 -0300

    Implementação do KenoGame.hpp + adaptação do KenoGame.cpp

--- include/KenoGame.hpp
+++ include/KenoGame.hpp
@@ -6,47 +6,62 @@
 #include "vector"
 
 using set_of_sets = std::vector< set_of_numbers_type >; //Conjunto dos conjuntos do tipo de dado do keno hit
-using set_of_wages = std::vector < cash_type >; //Conjunto de apostas do tipo definidas nesse aplicativo
+using set_of_wages = std::vector< cash_type >; //Conjunto de apostas do tipo definidas nesse aplicativo
 using indR = vector<number_type>::iterator;//Iterator para round da vez
 using indW = vector<cash_type>::iterator;//Iterator para wage do round da vez
 
 
 class KenoGame {
     private:
-        set_of_sets g_hits; //Números sorteados em todos os rounds
-        set_of_wages g_wages; //Apostas de todos os rounds
-        indR g_round; //Iterator indice do round da vez
-        indW g_wage; //Iterator indice da wage do round da vez
+        set_of_sets g_hits; //<! Números sorteados em todos os rounds
+        set_of_wages g_wages; //<! Apostas de todos os rounds
+        indR g_round; //<! Iterator indice do round da vez
+        indW g_wage; //<! Iterator indice da wage do round da vez
+        KenoBet kb; //<! Objeto da classe KenoBet
     public:
-        //Cria um KenoGame vazio usando um objeto da classe KenoBet
+        /*! Cria um KenoGame vazio usando um objeto da classe KenoBet
+            @param kb Um objeto do tipo KenoBet
+        */
         KenoGame(KenoBet kb);
 
-        //Coloca o iteretor indice do primeiro round
+        /*! Coloca o iteretor indice para o começo de g_hits
+            @param g_round_ O indice do começo de g_hits
+        */
         void put_round(indR g_round_);
 
-        //Retorna o iterator indice do round atual
+        /*! Retorna o iterator do round atual 
+            @return O iterator do round atual
+        */
         indR get_round();
 
-        //Coloca o iteretor indice da wage do primeiro round
+        /*! Coloca o iteretor para o começo de g_wages
+            @param g_round_wage_ O iterator do começo de g_wages
+        */
         void put_round_wage(indW g_round_wage_);
 
-        //Retorna o iterator indice da wage do round atual
+        /*! Retorna o iterator da wage do round atual 
+            @return O iterator apontando para a wage do round atual
+        */
         indW get_round_wage();
 
-        //Sorteia e preenche os números do round atual usando o iterator
-        void make_hits(indR g_round_);
+        //! Sorteia e preenche os números do round atual
+        void make_hits();
 
-        //Retorna os números sorteados do round da vez
-        set_of_numbers_type get_hits(indR g_round_);
+        /*! Retorna o vetor com os números sorteados do round atual
+            @return Vetor com os números sorteados do round atual
+        */
+        set_of_numbers_type get_round_hits();
 
-        //Coloca o valor da aposta do round atual
-        void put_wage(indW g_round_wage_);
+        //! Coloca o valor da aposta do round atual 
+        void put_wage();
 
-        //Retorna o valor da aposta do round atual
-        cash_type get_wage(indW g_round_wage);
+        /*! Retorna o vetor de valores de apostas 
+            @return Vetor de valores das apostas
+        */
+        set_of_wages get_g_wage();
 
-        //Calcula e adiciona ou remove da wage do round atual dependendo do número de matchs
-        void calc_wage(indR g_round_, indW g_round_wage_, set_of_numbers_type g_hits_, cash_type g_wage_);
+        //! Calcula e adiciona ou remove da wage do round atual dependendo do número de matchs
+        void calc_wage();
 };
 
 #endif //KenoGame_hpp
--- src/KenoGame.cpp
+++ src/KenoGame.cpp
@@ -1,1 +1,54 @@
 #include "KenoGame.hpp"
+#include "KenoBet.hpp"
+
+using namespace std;
+KenoGame::KenoGame(KenoBet kb_) {
+    kb_ = kb;
+}
+
+void KenoGame::put_round(indR g_round_){
+    g_round = g_round_;
+}
+
+indR KenoGame::get_round() {
+    return g_round;
+}
+
+void KenoGame::put_round_wage(indW g_round_wage_){
+    g_wage = g_round_wage_;
+}
+
+indW KenoGame::get_round_wage() {
+    return g_wage;
+}
+
+void KenoGame::make_hits() {
+    std::vector<number_type> x;
+    int count = 0;
+    srand(g_round); //Setar a seed para que o rand gere numeros diferentes, mas possa gerar numeros iguais em rounds diferentes
+    while(count != 20) {
+        number_type numb = rand() % 80 + 1;
+        x.push_back(numb);
+        count = count + 1;
+    }
+    g_hits.push_back(x);
+}
+
+set_of_numbers_type KenoGame::get_round_hits() {
+    return g_hits[g_round];
+}
+
+void KenoGame::put_wage() {
+    auto rw = kb.KenoBet::get_wage();
+    g_wages.push_back(rw);
+}
+
+set_of_wages KenoGame::get_g_wage() {
+    return g_wages;
+}
+
+//! Deixado para depois, quando receber resposta
+void KenoGame::calc_wage() {
+    ++g_round;
+    ++g_wage;
+}
