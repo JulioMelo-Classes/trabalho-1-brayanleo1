d98db628f11fff16d64124512258263ef4a6b05f
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-07-04 15:41:08 -0300

    Concerto em KenoGame.hpp

--- include/KenoGame.hpp
+++ include/KenoGame.hpp
@@ -7,38 +7,46 @@
 
 using set_of_sets = std::vector< set_of_numbers_type >; //Conjunto dos conjuntos do tipo de dado do keno hit
 using set_of_wages = std::vector < cash_type >; //Conjunto de apostas do tipo definidas nesse aplicativo
+using indR = vector<number_type>::iterator;//Iterator para round da vez
+using indW = vector<cash_type>::iterator;//Iterator para wage do round da vez
 
-template<class InputIt> //Iterator para a range
 
 class KenoGame {
     private:
         set_of_sets g_hits; //Números sorteados em todos os rounds
         set_of_wages g_wages; //Apostas de todos os rounds
-        InputIt g_round; //Iterator indice do round da vez
+        indR g_round; //Iterator indice do round da vez
+        indW g_wage;
     public:
         //Cria um KenoGame vazio usando uma objeto da classe KenoBet
         KenoGame(KenoBet kb);
 
         //Coloca o iteretor indice do primeiro round
-        void put_round(InputIt g_round_);
+        void put_round(indR g_round_);
 
         //Retorna o iterator indice do round atual
-        InputIt get_round();
+        indR get_round();
+
+        //Coloca o iteretor indice da wage do primeiro round
+        void put_round_wage(indW g_round_wage_);
+
+        //Retorna o iterator indice do round atual
+        indW get_round_wage();
 
         //Sorteia e preenche os números do round atual usando o iterator
-        void make_hits(InputIt g_round_);
+        void make_hits(indR g_round_);
 
         //Retorna os números sorteados do round da vez
-        set_of_numbers_type get_hits(InputIt g_round_);
+        set_of_numbers_type get_hits(indR g_round_);
 
         //Coloca o valor da aposta do round atual
-        void put_wage(InputIt g_round_);
+        void put_wage(indW g_round_wage_);
 
         //Retorna o valor da aposta do round atual
-        cash_type get_wage(InputIt g_round_);
+        cash_type get_wage(indW g_round_wage);
 
         //Calcula e adiciona ou remove da wage do round atual dependendo do número de matchs
-        void calc_wage(InputIt g_round_, set_of_numbers_type g_hits_, cash_type g_wage_);
+        void calc_wage(indR g_round_, indW g_round_wage_, set_of_numbers_type g_hits_, cash_type g_wage_);
 };
 
 #endif //KenoGame_hpp
